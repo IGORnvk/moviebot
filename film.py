48b6d1850294160452f37ac2539536b3e2af5e6e
user <igor.kn1911@gmail.com>
2020-09-26 13:12:34 +0300

    added del_film in FilmLibrary added __str__ in Film

--- film.py
+++ film.py
@@ -3,10 +3,12 @@ class Film:
         self.name = name
         self.rating = rating
 
+    def __str__(self):
+        return "film: %s, rating: %d" % (self.name, self.rating)
 
 class FilmLibrary:
     def __init__(self):
-        self.films = {}
+        self.films = dict()
 
     def set_film(self, film):
         self.films[film.name] = film
@@ -15,4 +17,7 @@ class FilmLibrary:
         return self.films[name]
 
     def get_films_info(self):
-        return self.films
+        return list(self.films.values())
+
+    def del_film(self, name):
+        del self.films[name]
---
+++ test.py
@@ -0,0 +1,15 @@
+import film
+
+film_library = film.FilmLibrary()
+film_1 = film.Film('abc', 1)
+film_2 = film.Film("dfg", 9)
+
+film_library.set_film(film_1)
+film_library.set_film(film_2)
+
+film_library.del_film(film_1.name)
+
+film_list = film_library.get_films_info()
+
+for film in film_list:
+    print(film)
